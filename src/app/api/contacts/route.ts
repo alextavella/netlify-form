8acac384a97e8f88f6110a30efcfd9d04e2ae583
alextavella <alex.tavella@azos.com.br>
2025-07-11 12:47:57 -0300

    refactor: use rest

--- src/app/api/contacts/route.ts
+++ src/app/api/contacts/route.ts
@@ -1,21 +1,26 @@
-import { netlifyClient } from '@/lib/netlify';
-
-const siteName = 'at-netlify-form';
+// const siteName = 'at-netlify-form';
+const siteId = '9658a9c0-e456-417a-a72c-518278cb634a';
 
 export async function GET(_: Request) {
-  const sites = await netlifyClient.listSites().catch(() => []);
-  const site_id = sites.find((site) => site.name === siteName)?.id;
+  // const sites = await netlifyClient.listSites().catch(() => []);
+  // const site_id = sites.find((site) => site.name === siteName)?.id;
 
-  if (!site_id) {
-    return new Response(
-      JSON.stringify({
-        error: 'Site not found',
-      }),
-      { status: 404 }
-    );
-  }
+  // if (!site_id) {
+  //   return new Response(
+  //     JSON.stringify({
+  //       error: 'Site not found',
+  //     }),
+  //     { status: 404 }
+  //   );
+  // }
 
-  const submissions = await netlifyClient.listSiteSubmissions({ site_id }).catch(() => []);
+  // const submissions = await netlifyClient.listSiteSubmissions({ site_id }).catch(() => []);
+
+  const submissions = await fetch(`https://api.netlify.com/api/v1/sites/${siteId}/submissions`, {
+    headers: {
+      Authorization: `Bearer ${process.env.NETLIFY_API_KEY}`,
+    },
+  }).then((res) => res.json());
 
   return new Response(JSON.stringify({ submissions }));
 }
